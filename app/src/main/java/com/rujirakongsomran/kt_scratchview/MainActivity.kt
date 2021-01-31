e27f2ea5fb46b2d647aab61c1828efc90eb58cf6
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-01-31 22:00:00 +0700

    Finish

--- app/src/main/java/com/rujirakongsomran/kt_scratchview/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_scratchview/MainActivity.kt
@@ -2,6 +2,9 @@ package com.rujirakongsomran.kt_scratchview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.anupkumarpanwar.scratchview.ScratchView
 import com.rujirakongsomran.kt_scratchview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        createScratchCard()
+    }
+
+    private fun createScratchCard() {
+        binding.sv.setRevealListener(object : ScratchView.IRevealListener {
+            override fun onRevealed(scratchView: ScratchView?) {
+                Toast.makeText(applicationContext, "Reveled", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onRevealPercentChangedListener(scratchView: ScratchView?, percent: Float) {
+                if (percent >= 0.5) {
+                    Log.d("Reveal Percentage", "onRevealPercentChangedListener: $percent");
+                }
+            }
+        })
     }
 }
