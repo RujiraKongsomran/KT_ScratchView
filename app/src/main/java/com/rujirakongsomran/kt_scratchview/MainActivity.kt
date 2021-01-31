18b1144a4e47d7e7df9779c206f55536b1e22872
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-01-31 18:13:41 +0700

    Enable ViewBinding

--- app/src/main/java/com/rujirakongsomran/kt_scratchview/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_scratchview/MainActivity.kt
@@ -2,10 +2,15 @@ package com.rujirakongsomran.kt_scratchview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rujirakongsomran.kt_scratchview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
